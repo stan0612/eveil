e3cf102fe97cb3356aea329b0d563ccb668d4b0a
Stanislas Grobien <sgrobien0@gmail.com>
2018-11-30 11:47:35 +0100

    yesyes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: 'products#index'
   devise_for :users
-  resources :products, only: [:show, :new, :create]
+  resources :products, only: [:index, :show, :new, :create]
   resources :orders, only: [:index, :create] do
     member do
       get 'pay', to: 'orders#pay'
