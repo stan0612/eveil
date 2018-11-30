f969d8e334632b8ba6cbc76cc1a51dedbd0d6497
Stanislas Grobien <sgrobien0@gmail.com>
2018-11-30 11:44:25 +0100

    fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: 'products#index'
   devise_for :users
-  resources :products, only: [:show, :new, :create]
+  resources :products, only: [:index, :show, :new, :create]
   get 'add_reviews', to: 'reviews#add_reviews'
   resources :reviews, only: [:create]
   resources :orders, only: [:index, :create] do
